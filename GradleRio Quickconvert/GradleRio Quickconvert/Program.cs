bffda07b7364881c0e4d9720818a853dd5238e8d
MoSadie <sean@cushmantech.net>
2018-06-01 22:48:12 -0700

    Functionally Complete.
    Now to make it neater and add an icon.

--- GradleRio Quickconvert/GradleRio Quickconvert/Program.cs
+++ GradleRio Quickconvert/GradleRio Quickconvert/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 using System.IO.Compression;
+using System.Diagnostics;
 
 namespace GradleRio_Quickconvert
 {
@@ -43,13 +44,13 @@ namespace GradleRio_Quickconvert
             Console.WriteLine("Project found!");
             Console.WriteLine();
             Console.WriteLine("Presetup complete!");
-            Console.WriteLine("What is the main package of your robot code? (ex. Team4450.Robot11)");
+            Console.WriteLine("What is your team number? (ex. 4450)");
 
-            string robotPackage = Console.ReadLine();
+            string teamNumber = Console.ReadLine();
 
             Console.WriteLine();
 
-            Console.WriteLine("What is the main robot class? (ex. Robot)");
+            Console.WriteLine("What is the main robot class? (ex. Team4450.Robot11.Robot)");
 
             string robotClass = Console.ReadLine();
 
@@ -57,9 +58,21 @@ namespace GradleRio_Quickconvert
 
             DirectoryInfo projectRoot = new DirectoryInfo(System.AppDomain.CurrentDomain.BaseDirectory);
 
-            Console.WriteLine("Getting GradleRio Quickstart for Java.");
+            Console.WriteLine("Moving source files");
 
-            if (!getGradleRioQuickstart())
+            Directory.Move("src", "java");
+            Directory.CreateDirectory("src/main");
+            Directory.Move("java", "src/main/java");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Source files moved!");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Getting GradleRio setup.");
+
+            if (!getGradleRioSetup(teamNumber, robotClass))
             {
                 Console.WriteLine("Something went wrong getting the quickstart zip.");
                 Console.WriteLine("Please try again later. Press any key to exit.");
@@ -67,17 +80,65 @@ namespace GradleRio_Quickconvert
                 System.Environment.Exit(0);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("GradleRio setup complete!");
+            Console.WriteLine("Now cleaning up eclipse project files.");
+
+            if (!cleanEclipseProject())
+            {
+                Console.WriteLine("Something went wrong cleaning up the eclipse project.");
+                Console.WriteLine("Please try again later. Press any key to exit.");
+                Console.ReadKey(true);
+                System.Environment.Exit(0);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Cleanup complete!");
+            Console.WriteLine("Attempting to build the project.");
+
+            if (!buildProject())
+            {
+                Console.WriteLine("Something went wrong attempting to building the project.");
+                Console.WriteLine("Please try again later. Press any key to exit.");
+                Console.ReadKey(true);
+                System.Environment.Exit(0);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Test build complete! Now creating new eclipse project.");
+
+            if (!createEclipseProject())
+            {
+                Console.WriteLine("Something went wrong creating the new eclipse project.");
+                Console.WriteLine("Please try again later. Press any key to exit.");
+                Console.ReadKey(true);
+                System.Environment.Exit(0);
+            }
+
+            Console.WriteLine();
+
+            if (File.Exists(".gitignore"))
+            {
+                Console.WriteLine("Adding cache folders to .gitignore");
+                File.AppendAllLines(".gitignore", new string[] { "", "# Gradle cache", ".gradle", "", "# Eclipse settings", ".settings" });
+            }
+
+            Console.WriteLine("Everything's all setup! Cleaning up tmp files, including this program!");
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
+
+            Directory.Delete("tmp", true);
+            File.Delete(System.Reflection.Assembly.GetEntryAssembly().Location);
         }
 
         static bool internetCheck()
         {
             try
             {
-                HttpWebRequest request = HttpWebRequest.CreateHttp("https://github.com/MoSadie/"); //TODO UPDATE THIS
+                HttpWebRequest request = HttpWebRequest.CreateHttp("https://github.com/MoSadie/GradleRioQuickconvert");
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 return (response.StatusCode == HttpStatusCode.OK);
-            } catch (WebException e)
+            } catch
             {
                 return false;
             }
@@ -111,20 +172,98 @@ namespace GradleRio_Quickconvert
             return result;
         }
 
-        static bool getGradleRioQuickstart()
+        static bool getGradleRioSetup(string teamNumber, string robotClass)
         {
             try
             {
+                Directory.CreateDirectory("tmp");
+
                 using (var client = new WebClient())
                 {
-                    client.DownloadFile("https://github.com/Open-RIO/GradleRIO/blob/master/Quickstart.zip?raw=true", "tmp/Quickstart.zip");
+                    client.DownloadFile("https://github.com/MoSadie/GradleRioQuickconvert/blob/master/DownloadPackage.zip?raw=true", "tmp/DownloadPackage.zip");
                 }
 
-                using (var zip = new ZipArchive("tmp/Quickstart.zip"))
+                ZipFile.ExtractToDirectory("tmp/DownloadPackage.zip","tmp");
+                //Directory.Move("tmp/DownloadPackage", System.AppDomain.CurrentDomain.BaseDirectory);
+
+                Directory.Move("tmp/DownloadPackage/.vscode", ".vscode");
+                Directory.Move("tmp/DownloadPackage/gradle", "gradle");
+
+                foreach (string file in Directory.GetFiles("tmp/DownloadPackage"))
                 {
-
+                    string newFile = file.Replace("tmp/DownloadPackage\\", "");
+                    File.Move(file, newFile);
                 }
+
+                if (File.Exists("build.gradle"))
+                {
+                    File.Move("build.gradle", "build.gradle.backup");
+                }
+
+                using (var stream = File.CreateText("build.gradle"))
+                {
+                    for (int i = 0; i < Util.buildGradleArray.Length; i++)
+                    {
+                        if (i == Util.RobotClassIndex)
+                        {
+                            stream.WriteLine(Util.buildGradleArray[Util.RobotClassIndex] + robotClass + "\"");
+                        } else if (i == Util.TeamNumberIndex)
+                        {
+                            stream.WriteLine(Util.buildGradleArray[Util.TeamNumberIndex] + teamNumber);
+                        } else
+                        {
+                            stream.WriteLine(Util.buildGradleArray[i]);
+                        }
+                    }
+                }
+
+                return true;
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        static bool cleanEclipseProject()
+        {
+            try
+            {
+                if (File.Exists(".project")) File.Delete(".project");
+                if (File.Exists(".classpath")) File.Delete(".classpath");
+                if (File.Exists("build.xml")) File.Delete("build.xml");
+                if (File.Exists("build.properties")) File.Delete("build.properties");
+
+                return true;
             } catch
+            {
+                return false;
+            }
+        }
+
+        static bool buildProject()
+        {
+            try
+            {
+                var process = Process.Start("gradlew.bat", "build");
+                process.WaitForExit();
+                return true;
+            } catch
+            {
+                return false;
+            }
+        }
+
+        static bool createEclipseProject()
+        {
+            try
+            {
+                var process = Process.Start("gradlew.bat", "eclipse");
+                process.WaitForExit();
+                return true;
+            }
+            catch
             {
                 return false;
             }
